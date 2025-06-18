74ba9285ecdc601a09ad547eab9f6bf2002548c8
kliklis <kklimantakis@gmail.com>
2025-06-18 12:28:17 +0300

    2nd page fixed. [2025-06-18_12-27-50]

---
+++ Scripts/loadScript.js
@@ -0,0 +1,9 @@
+function loadScript(src) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.src = src;
+    script.onload = () => resolve(src);
+    script.onerror = () => reject(new Error(`Failed to load: ${src}`));
+    document.head.appendChild(script);
+  });
+}
--- Scripts/scenario.js
+++ Scripts/scenario.js
@@ -1,17 +1,20 @@
-getIP().then(ip => {
-  if (ip) {
-    showInstructions(3, `Hello ${ip}!`);
+function runScenario() {
+	
+	getIP().then(ip => {
+	  if (ip) {
+		showInstructions(3, `Hello ${ip}!`);
 
-    const playerClass = getPlayerIndex(ip, 6);
-    showInstructions(4, `You belong to Player Class: ${playerClass}.`);
+		const playerClass = getPlayerIndex(ip, 6);
+		showInstructions(4, `You belong to Player Class: ${playerClass}.`);
 
-  } else {
-    console.log("Connection Error!");
-    showInstructions(3, "Hello Player!");
-  }
-});
+	  } else {
+		console.log("Connection Error!");
+		showInstructions(3, "Hello Player!");
+	  }
+	});
 
-showInstructions(5, "We run some last minute checks for you...");
-showInstructions(6, "This may take a while...");
-showInstructions(50, "Redirection in 10 seconds!");
-timedRedirection(60, "https://nobudgetstudios.github.io/MechanisObscuraDirective_Website/giraffe.html");
+	showInstructions(5, "We run some last minute checks for you...");
+	showInstructions(6, "This may take a while...");
+	showInstructions(50, "Redirection in 10 seconds!");
+	timedRedirection(60, "https://nobudgetstudios.github.io/MechanisObscuraDirective_Website/giraffe.html");
+}
